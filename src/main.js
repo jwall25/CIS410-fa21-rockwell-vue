b7a84254b3acae43dc9b9814dba3bdb58dc4f855
unknown <jwall25@colostate.edu>
2021-12-07 22:48:24 -0700

    12/7 signup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import axios from "axios";
 import myRouter from "./routes.js";
+
+axios.defaults.baseURL = "https://cis410-fa21-wallace-test.azurewebsites.net";
 
 const myApp = createApp(App);
 
