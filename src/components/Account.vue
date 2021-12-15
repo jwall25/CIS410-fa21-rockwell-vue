13c5f3b7e18881995c63edf51ad56bbb99bde59a
unknown <jwall25@colostate.edu>
2021-12-14 20:46:13 -0700

    12/14 Account

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,11 +1,65 @@
 <template>
   <div>
     <h1>Account</h1>
+    <hr />
+    <h3>{{ firstName }}'s Reviews</h3>
+
+    <p v-if="accountError" class="text-danger">
+      Can not get your account information. Please try again Later
+    </p>
+
+    <table v-if="reviewsByUser" class="table">
+      <thead>
+        <th>Title</th>
+        <th>Summary</th>
+        <th>Rating</th>
+      </thead>
+      <tbody>
+        <tr v-for="thisReview in reviewsByUser" :key="thisReview.ReviewPK">
+          <th>
+            <router-link :to="`/movies/${thisReview.MovieFK}`">{{
+              thisReview.Title
+            }}</router-link>
+          </th>
+          <th>{{ thisReview.Summary }}</th>
+          <th>{{ thisReview.Rating }}</th>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      reviewsByUser: null,
+      accountError: false,
+    };
+  },
+  computed: {
+    firstName() {
+      console.log("Here is store so far:", this.$store.state);
+      return this.$store.state.user.NameFirst;
+    },
+  },
+  created() {
+    axios
+      .get("/reviews/me", {
+        headers: {
+          Authorization: `Bearer ${this.$store.state.token}`,
+        },
+      })
+      .then((theResponse) => {
+        // console.log("Response", theResponse);
+        this.reveiwsByUser = theResponse.data;
+      })
+      .catch(() => {
+        this.accountError = true;
+      });
+  },
+};
 </script>
 
 <style></style>
